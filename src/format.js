c942c116d6bb67270215261fbf78eb969bbdf234
lymava <lymava@sina.com>
2015-09-10 15:21:44 +0800

    测试

---
+++ src/format.js
@@ -0,0 +1,37 @@
+var format = {
+	"data" : [ {
+		"id" : "5540a44dd812a83f8eed3c86",
+		"name" : "门窗",
+		"child" : [ {
+			"id" : "5540a47ed812a83f8eed3c88",
+			"name" : "推拉门窗"
+		}, {
+			"id" : "5540a47ed812a83f8eed3c87",
+			"name" : "平开门窗"
+		}, {
+			"id" : "55482668d6c4591248857578",
+			"name" : "铝木复合门窗"
+		} ]
+	}, {
+		"id" : "5540a368d812a83f8eed3c7c",
+		"name" : "栏杆",
+		"child" : []
+	}, {
+		"id" : "5540a35cd812a83f8eed3c7b",
+		"name" : "阳光房",
+		"child" : []
+	}, {
+		"id" : "5540a357d812a83f8eed3c7a",
+		"name" : "豪华艺术门",
+		"child" : []
+	}, {
+		"id" : "5540a352d812a83f8eed3c79",
+		"name" : "六角亭",
+		"child" : []
+	}, {
+		"id" : "5540a34bd812a83f8eed3c78",
+		"name" : "葡萄架",
+		"child" : []
+	} ],
+	"state" : 2
+};
