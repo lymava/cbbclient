c8ef898b2e4a430c9b907f868aa11dcc93514297
lymava <lymava@sina.com>
2015-09-28 10:21:57 +0800

    format

--- src/format.js
+++ src/format.js
@@ -1,26 +1,79 @@
 var format = {
-	"_id" : {
-		"$oid" : "55f66577d105fc2e4efb8acf"
-	},
-	"a" : "4fa53290d3dc4820059e44ef",
-	"b" : "欧莱雅官方旗舰店彩妆正品 气垫BB霜美白保湿遮瑕强裸妆隔离cc霜",
-	"c" : "23号 淘金币价 仅此一天 送5件套 送完即止",
-	"d" : 1441865791000,
-	"f" : 1,
-	"g" : "使用常识",
-	"h" : "/attachFiles/temp/55f6655fd105fc2e4efb8aca",
-	"i" : [ "/attachFiles/temp/55f66562d105fc2e4efb8acb",
-			"/attachFiles/temp/55f66564d105fc2e4efb8acc",
-			"/attachFiles/temp/55f66566d105fc2e4efb8acd",
-			"/attachFiles/temp/55f66569d105fc2e4efb8ace" ],
-	"j" : "商品详情",
-	"k" : "55f65c16d105fc2d956fc5c4",
-	"l" : 1,
-	"m" : 2,
-	"n" : "",
-	"o2" : "5513b910d812a8327f224de5",
-	"priceFen" : 360,
-	"yuanjiaFen" : 390,
-	"startSaleTime" : 1442211384000,
-	"e" : 1
+	"data" : [ {
+		"id" : "55fbdf26d105fc3bac4b0622",
+		"name" : "系统消息",
+		"content" : "gfdiasohgiosa;hgdsklahfjdisahgudfhsgfdsuhafdjsa",
+		"state" : "3",
+		"time" : "1442570022000"
+	}, {
+		"id" : "55fbdf26d105fc3bac4b0621",
+		"name" : "系统消息",
+		"content" : "gfdiasohgiosa;hgdsklahfjdisahgudfhsgfdsuhafdjsa",
+		"state" : "3",
+		"time" : "1442570022000"
+	}, {
+		"id" : "55fbdf26d105fc3bac4b0620",
+		"name" : "系统消息",
+		"content" : "gfdiasohgiosa;hgdsklahfjdisahgudfhsgfdsuhafdjsa",
+		"state" : "3",
+		"time" : "1442570022000"
+	}, {
+		"id" : "55fbdf26d105fc3bac4b061f",
+		"name" : "系统消息",
+		"content" : "gfdiasohgiosa;hgdsklahfjdisahgudfhsgfdsuhafdjsa",
+		"state" : "3",
+		"time" : "1442570022000"
+	}, {
+		"id" : "55fbdf26d105fc3bac4b061e",
+		"name" : "系统消息",
+		"content" : "gfdiasohgiosa;hgdsklahfjdisahgudfhsgfdsuhafdjsa",
+		"state" : "3",
+		"time" : "1442570022000"
+	}, {
+		"id" : "55fbdf25d105fc3bac4b061d",
+		"name" : "系统消息",
+		"content" : "gfdiasohgiosa;hgdsklahfjdisahgudfhsgfdsuhafdjsa",
+		"state" : "3",
+		"time" : "1442570021000"
+	}, {
+		"id" : "55fbdf1fd105fc3bac4b061c",
+		"name" : "系统消息",
+		"content" : "gfdiasohgiosa;hgdsklahfjdisahgudfhsgfdsuhafdjsa",
+		"state" : "3",
+		"time" : "1442570015000"
+	}, {
+		"id" : "55fbdf1fd105fc3bac4b061b",
+		"name" : "系统消息",
+		"content" : "gfdiasohgiosa;hgdsklahfjdisahgudfhsgfdsuhafdjsa",
+		"state" : "3",
+		"time" : "1442570015000"
+	}, {
+		"id" : "55fbdf1fd105fc3bac4b061a",
+		"name" : "系统消息",
+		"content" : "gfdiasohgiosa;hgdsklahfjdisahgudfhsgfdsuhafdjsa",
+		"state" : "3",
+		"time" : "1442570015000"
+	}, {
+		"id" : "55fbdf1fd105fc3bac4b0619",
+		"name" : "系统消息",
+		"content" : "gfdiasohgiosa;hgdsklahfjdisahgudfhsgfdsuhafdjsa",
+		"state" : "3",
+		"time" : "1442570015000"
+	}, {
+		"id" : "55fbdf1fd105fc3bac4b0618",
+		"name" : "系统消息",
+		"content" : "gfdiasohgiosa;hgdsklahfjdisahgudfhsgfdsuhafdjsa",
+		"state" : "3",
+		"time" : "1442570015000"
+	}, {
+		"id" : "55fbdf1fd105fc3bac4b0617",
+		"name" : "系统消息",
+		"content" : "gfdiasohgiosa;hgdsklahfjdisahgudfhsgfdsuhafdjsa",
+		"state" : "3",
+		"time" : "1442570015000"
+	} ],
+	"count" : 12,
+	"lastpage" : 1,
+	"state" : 2,
+	"description" : "查询成功"
 }
